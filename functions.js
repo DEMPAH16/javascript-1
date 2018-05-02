b7fe5dd06cd63cc4d7e8cd890105570f1caec26b
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2018-05-01 20:38:51 -0600

    example ready

---
+++ functions.js
@@ -0,0 +1,83 @@
+'use strict';
+
+function helloWorld() {
+    console.log('Hello, World!');
+}
+
+helloWorld();
+
+function greeting(name) {
+    blah = 'assigned';
+    console.log('Hello, ' + name + '!');
+}
+
+greeting('Taylor');
+greeting('Brad');
+greeting('Simon');
+greeting('Devin');
+greeting('et al');
+
+function makeGreeting(name) {
+    return 'Hello, ' + name + '!';
+}
+
+var taylorGreeting = makeGreeting('Taylor');//?
+
+
+taylorGreeting
+
+function makeIntlGreeting(name, lang/*, ...args*/) {
+    // console.log(args);
+    // console.log(arguments);
+    if (lang == 'en') {
+        // blah = 45;
+        let englishGreeting = 'Hello, ' + name + '!';
+        return englishGreeting;
+    }
+    else if (lang == 'sp' || lang == 'es') {
+        return 'Hola, ' + name + '!';
+    }
+    else if (lang == 'ru') {
+        return 'Privyet, ' + name + '!';
+    }
+    
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    
+    // i//? // Error!
+    
+    // englishGreeting;
+    
+    foo = 42;
+    
+    foo;//?
+    
+    var foo = 'bar';//?
+    
+    secondIntlGreeting();
+    
+    function secondIntlGreeting() {
+        // var foo;
+        
+        foo
+        
+        console.log(name, lang);
+    }
+    
+    return 'This kind of greeting hasn\'t been implemented yet\t \n';
+}
+
+// name;
+// lang;
+
+var simon = 'Simon';
+
+makeIntlGreeting(simon, 'en');//?
+makeIntlGreeting(simon, 'es');//?
+makeIntlGreeting(simon, 'sp');//?
+makeIntlGreeting(simon, 'ru')
+console.log(makeIntlGreeting(simon, 'fr'));
+
+
+blah
---
+++ variables.js
@@ -0,0 +1,52 @@
+var container = 'data';//?
+
+container = 42;//?
+
+container = false || true;//?
+
+container = {
+    name: 'desk',
+    legs: 4,
+    colors: [
+        'brown',
+        'black',
+    ],
+    length: 8,
+    width: 3,
+};//?
+
+container = [
+    'data',
+    42,
+    false || true,
+    {},
+    [],
+    () => {},
+    new Date(),
+];//?
+
+container = () => {};//?
+container = function () {};//?
+
+container = new Date();//?
+
+
+
+
+add;//?
+// add2;//?
+// add3;//?
+
+var add = '+';
+let add2;
+const theStoryOfMyLife = `obligatory assignment :'(`;
+const obj = {};
+
+obj.foo = 'bar';
+obj.arr = [];
+
+
+add;//?
+obj;//?
+
+// add3 = ''; // Error!
